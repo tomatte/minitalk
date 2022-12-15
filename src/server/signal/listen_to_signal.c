de49a7527934766552ada86e22e0d3fabcca073b
tomatte <daniel.brandao2000@gmail.com>
2022-12-15 16:54:53 -0300

    sends a confirmation for every bit received

--- src/server/signal/listen_to_signal.c
+++ src/server/signal/listen_to_signal.c
@@ -6,17 +6,18 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 13:52:32 by dbrandao          #+#    #+#             */
-/*   Updated: 2022/12/14 11:29:55 by dbrandao         ###   ########.fr       */
+/*   Updated: 2022/12/15 16:37:13 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../server.h"
 
-void	signal_handling(int signum)
+void	signal_handling(int signum, siginfo_t *info, void *ucontext)
 {
 	static char	c = 0;
 	static char	i = 0;
 
+	usleep(50);
 	i++;
 	if (signum == SIGUSR2)
 		c += ft_pow(2, 8 - i);
@@ -26,12 +27,17 @@ void	signal_handling(int signum)
 		c = 0;
 		i = 0;
 	}
+	kill(info->si_pid, SIGUSR1);
 }
 
 void	listen_to_signal()
 {
-	signal(SIGUSR1, signal_handling);
-	signal(SIGUSR2, signal_handling);
+	struct sigaction sa;
+
+	sa.sa_sigaction = signal_handling;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
-		;
+		usleep(50) ;
 }
