40ebc00c61cdbf8d891058afe6f1b4317f9b3363
Daniel <daniel.brandao2000@gmail.com>
2022-12-16 23:29:24 +0100

    argument verification

--- src/server/server.c
+++ src/server/server.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 22:05:48 by dbrandao          #+#    #+#             */
-/*   Updated: 2022/12/16 15:22:01 by coder            ###   ########.fr       */
+/*   Updated: 2022/12/16 23:06:42 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,14 @@ static void	print_pid(void)
 	ft_printf("Server pid: %d\n", pid);
 }
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
+	(void) argv;
+	if (argc > 1)
+	{
+		ft_printf("No argument is accepted. Program exited.\n");
+		exit(0);
+	}
 	print_pid();
 	listen_to_signal();
 	return (0);
